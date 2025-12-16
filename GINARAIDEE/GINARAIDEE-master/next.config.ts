69d63adeb2055ef2f4e4b56c747eccc3edc373f9
vorapol anananusit <93755191+Vorapol2004@users.noreply.github.com>
2025-12-16 18:27:43 +0700

    Delete GINARAIDEE directory

--- GINARAIDEE/GINARAIDEE-master/next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig = {
-    experimental: {
-        allowedDevOrigins: ["http://localhost:3000", "http://192.168.1.234:3000"],
-    },
-};
-
-export default nextConfig;
